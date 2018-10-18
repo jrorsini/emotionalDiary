bd92ac1df5747057a8d4b9416ec42bdcc9b8d503
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-18 23:37:08 +0200

    minor.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
-mongoose.connect(keys.mongoURI);
+mongoose
+	.connect(
+		keys.mongoURI,
+		{ useNewUrlParser: true }
+	)
+	.then((arg1, arg2) => {})
+	.catch(err => console.log(err));
 
 const Cat = mongoose.model('Cat', { name: String });
 
