caeef04bbd582e07f1c47ca324e942412dc21fbc
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-18 17:25:16 +0200

    Connected to database.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
-mongoose.connect(
-	keys.mongoURI,
-	{ useNewUrlParser: true }
-);
+mongoose.connect(keys.mongoURI);
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
