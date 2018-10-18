8cffb1cd7aec2ce7c3ebfe5271e014f7b337c26e
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-18 23:37:54 +0200

    minor.

--- index.js
+++ index.js
@@ -8,8 +8,3 @@ mongoose
 	)
 	.then((arg1, arg2) => {})
 	.catch(err => console.log(err));
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
