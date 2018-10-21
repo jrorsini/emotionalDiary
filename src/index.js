d7a86c9edbc58f1469e351f12668e6845b63817d
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-21 19:50:54 +0200

    minor.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById('root')
+	);
+};
+
+render(App);
+
+// webpack Hot Module Replacement API
+if (module.hot) {
+	module.hot.accept('./App', () => {
+		// if you are using harmony modules ({modules:false})
+		render(App);
+		// in all other cases - re-require App manually
+		render(require('./App'));
+	});
+}
