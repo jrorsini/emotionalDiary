daef4e21cbce9ae2ea1cbb8a4753177794dbc276
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-27 15:11:38 +0200

    Connecting to localmongo databse.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const keys = require('./keys');
 
 mongoose
-	.connect(keys.mongoURI)
-	.then((arg1, arg2) => {})
+	.connect(
+		keys.mongoURI,
+		{ useNewUrlParser: true }
+	)
+	.then(() => {
+		console.log('connected');
+	})
 	.catch(err => console.log(err));
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
