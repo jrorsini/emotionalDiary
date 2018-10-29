88d0d1222efff9c5468f57fbafeb9aa365b46862
Jean-Roger <jeanroger.orsini@gmail.com>
2018-10-29 03:12:50 +0100

    minor.

--- src/exercice1.js
+++ src/exercice1.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
+import { hot } from 'react-hot-loader';
 
 const options = [
 	{ value: 'chocolate', label: 'Chocolate' },
 	{ value: 'strawberry', label: 'Strawberry' },
 	{ value: 'vanilla', label: 'Vanilla' }
 ];
+
+const setOptions = optionString =>
+	optionString.split(' ').map(option => ({
+		value: option,
+		label: option.charAt(0).toUpperCase() + option.slice(1)
+	}));
 
 class Exercice1 extends Component {
 	constructor(props) {
@@ -16,19 +23,93 @@ class Exercice1 extends Component {
 				affect: 'Kind of affect',
 				emotion: 'Feeling right now'
 			},
+			activation: '',
+			affect: '',
+			emotion: '',
+			trigger: '',
 			entries: [],
 			triggers: [],
 			errorMessage: null
 		};
+		this.setEmotionOptions = this.setEmotionOptions.bind(this);
+	}
+
+	setEmotionOptions(activation, affect) {
+		const high_neutral = 'excited surprised rapt active';
+		const normal_neutral = 'happy delighted glad joyful hearty satisfied';
+		const normal_positive = 'happy delighted glad joyful hearty satisfied';
+		const high_positive =
+			'enthusiastic cheerful excited euphoric animated peppy';
+		const high_negative =
+			'jealous disgusted confused stressed angry frightened upset jittery concerned contradictory alarmed furious';
+
+		switch (activation) {
+			case 'high':
+				switch (affect) {
+					case 'positive':
+						return setOptions(high_positive);
+					case 'neutral':
+						return setOptions(high_neutral);
+					case 'negative':
+						return setOptions(high_negative);
+				}
+				break;
+			case 'normal':
+				switch (affect) {
+					case 'positive':
+						return setOptions(normal_positive);
+					case 'negative':
+						return setOptions(normal_neutral);
+				}
+				break;
+			case 'low':
+				switch (affect) {
+					case 'positive':
+						break;
+					case 'neutral':
+						break;
+					case 'negative':
+						break;
+				}
+				break;
+		}
 	}
 
 	render() {
+		const { activation, affect, emotion, trigger } = this.state;
+
 		return (
 			<div>
 				Select form goes here
-				<Select options={options} />
+				<Select
+					name="activationLevel"
+					placeholder="Activation level"
+					value={activation}
+					options={setOptions('high normal low')}
+				/>
+				{/* <Select
+					name="AffectType"
+					placeholder="Kind of affect"
+					value={affect}
+					options={
+						activation &&
+						(activation.value === 'normal'
+							? setOptions('positive negative')
+							: setOptions('positive neutral negative'))
+					}
+				/>
+				<Select
+					name="emotion"
+					placeholder="Feeling right now"
+					value={emotion}
+					options={
+						activation &&
+						affect &&
+						this.setEmotionOptions(activation.value, affect.value)
+					}
+				/> */}
 			</div>
 		);
 	}
 }
-export default Exercice1;
+export default hot(module)(Exercice1);
